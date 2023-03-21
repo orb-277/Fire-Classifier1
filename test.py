c2082bf2a8f1da24023fc2c14f827c95c3cfa056
orb-277 <omkarbhosale277@gmail.com>
2023-03-21 10:11:14 +0530

    test1

--- test.py
+++ test.py
@@ -2,8 +2,21 @@ import tensorflow as tf
 import os
 import numpy as np
 
+import matplotlib.pyplot as plt
+
 from skimage.io import imread,imshow
 
 model = tf.keras.models.load_model('Modelv1')
 
-model.predict
+data = np.load('data.npz')
+X_test = data['X_test']
+Y_test = data['Y_test']
+
+
+
+imshow(X_test[0])
+plt.show()
+imshow(np.squeeze(Y_test[0]))
+plt.show()
+imshow(np.squeeze(model.predict(X_test[0])))
+plt.show()
