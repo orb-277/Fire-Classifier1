92609a797fbd92ca4891b173442fac485025f197
orb-277 <omkarbhosale277@gmail.com>
2023-03-26 01:22:24 +0530

    yooo

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import os
 import numpy as np
+import random
 
 import matplotlib.pyplot as plt
 
@@ -9,14 +10,22 @@ from skimage.io import imread,imshow
 model = tf.keras.models.load_model('Modelv1')
 
 data = np.load('data.npz')
+X_train = data['X_train']
 X_test = data['X_test']
 Y_test = data['Y_test']
 
+preds_test = model.predict(X_test, verbose=1)
+preds_test = (preds_test > 0.5).astype(np.uint8)
 
 
-imshow(X_test[0])
+
+
+ix = random.randint(0, len(X_test))
+imshow(X_test[ix])
 plt.show()
-imshow(np.squeeze(Y_test[0]))
+imshow(np.squeeze(Y_test[ix]))
 plt.show()
-imshow(np.squeeze(model.predict(X_test[0])))
+imshow(np.squeeze(preds_test[ix]))
 plt.show()
+
+print(np.squeeze(preds_test[ix]))
