846fb7feb7b4701e1d52d790099d1d62f7d5371d
orb-277 <omkarbhosale277@gmail.com>
2023-04-09 10:16:09 +0530

    Contoruing

--- test.py
+++ test.py
@@ -2,10 +2,13 @@ import tensorflow as tf
 import os
 import numpy as np
 import random
+import cv2 as cv
 
-import matplotlib.pyplot as plt
 
-from skimage.io import imread,imshow
+
+##import matplotlib.pyplot as plt
+
+##from skimage.io import imread,imshow
 
 model = tf.keras.models.load_model('Modelv1')
 
@@ -14,18 +17,46 @@ X_train = data['X_train']
 X_test = data['X_test']
 Y_test = data['Y_test']
 
-preds_test = model.predict(X_test, verbose=1)
+preds_test = model.predict(X_test)
 preds_test = (preds_test > 0.5).astype(np.uint8)
 
 
 
 
 ix = random.randint(0, len(X_test))
-imshow(X_test[ix])
-plt.show()
-imshow(np.squeeze(Y_test[ix]))
-plt.show()
-imshow(np.squeeze(preds_test[ix]))
-plt.show()
 
-print(np.squeeze(preds_test[ix]))
+
+
+
+# img = cv.cvtColor(Y_test[ix], cv.COLOR_BGR2GRAY)
+# cv.imshow('Y',img) 
+# cv.waitKey(0)
+	
+
+
+
+
+
+cv.imshow('X',np.array(X_test[ix]).astype('uint8'))
+cv.waitKey(0)
+
+cv.imshow('XP',np.array(preds_test[ix]).astype('uint8'))
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+
+
+
+
+
+
+cv.imshow('preds',preds_test[ix])
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+# cv.imshow(np.mat(np.squeeze(Y_test[ix])))
+
+# cv.imshow(np.mat(np.squeeze(preds_test[ix])))
+
+
+
